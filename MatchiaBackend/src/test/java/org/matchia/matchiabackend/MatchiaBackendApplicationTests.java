e6443409f87b1a499f2777a54563857d62cacb26
Mouakhar Yasmine <mouakharyasmine@gmail.com>
2026-08-19 13:19:40 +0100

    Add PostgreSQL Testcontainers for backend tests

--- MatchiaBackend/src/test/java/org/matchia/matchiabackend/MatchiaBackendApplicationTests.java
+++ MatchiaBackend/src/test/java/org/matchia/matchiabackend/MatchiaBackendApplicationTests.java
@@ -2,12 +2,24 @@ package org.matchia.matchiabackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
 @SpringBootTest
 class MatchiaBackendApplicationTests {
+
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres =
+            new PostgreSQLContainer<>("postgres:18")
+                    .withDatabaseName("matchia_test")
+                    .withUsername("test")
+                    .withPassword("test");
 
     @Test
     void contextLoads() {
     }
-
 }
